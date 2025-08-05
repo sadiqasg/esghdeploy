e48cfc344dcfedcde2ae805f4a06067498b1fda9
sadiqasg <sadiqasg@gmail.com>
2025-08-05 20:04:05 +0100

    removed all schema files

--- prisma/schema/migrations/20250803221342_add_department_to_user/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."users" ADD COLUMN     "departmentId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "public"."users" ADD CONSTRAINT "users_departmentId_fkey" FOREIGN KEY ("departmentId") REFERENCES "public"."Department"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/schema/migrations/20250805172052_added_unique_to_subscription_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name]` on the table `subscription` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "subscription_name_key" ON "public"."subscription"("name");
