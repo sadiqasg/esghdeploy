ebe42512f3fc2288535316c2bd2f8cf470a7ce99
sadiqasg <sadiqasg@gmail.com>
2025-08-06 17:39:59 +0100

    added cors handling

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,50 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    const allowedOrigins =
+        configService.get<string>("ALLOWED_ORIGINS")?.split(",") || [];
 
-  const config = new DocumentBuilder()
-    .setTitle('ESG Horizon API')
-    .setDescription('API documentation for the horizon services')
-    .setVersion('0.1')
-    .addBearerAuth()
-    .build();
+    app.enableCors({
+        origin: (
+            origin: string | undefined,
+            callback: (err: Error | null, allow?: boolean) => void
+        ) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
+        credentials: true,
+    });
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        })
+    );
 
-  await app.listen(process.env.PORT ?? 3000);
+    const config = new DocumentBuilder()
+        .setTitle("ESG Horizon API")
+        .setDescription("API documentation for the horizon services")
+        .setVersion("0.1")
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
