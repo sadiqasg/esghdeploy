c58acbfa72df37e059b671982774a3dbe1e65557
sadiqasg <sadiqasg@gmail.com>
2025-08-15 02:10:05 +0100

    unify schema

--- prisma/schema/migrations/20250814103904_lead_string_to_id/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `lead` on the `Department` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Department" DROP COLUMN "lead",
-ADD COLUMN     "lead" INTEGER NOT NULL;
